241a4b10f1e9fbcfba0db371f5c2c9cf024f2529
Oliver Welsh <104843826+ogb-Welsh@users.noreply.github.com>
2025-03-20 22:59:19 +0200

    Created a signle run file for all models. Still needs to be improved.

---
+++ run.py
@@ -0,0 +1,109 @@
+#! /usr/bin/python3
+
+import argparse
+import os
+import torch
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
+
+from utils import *
+
+if __name__ == "__main__":
+    # Accept config file as command parameter
+    parser = argparse.ArgumentParser(description="Load YAML configuration file.")
+    parser.add_argument(
+        "config_file",
+        nargs="?",
+        default="base_model_1.yaml",
+        type=str,
+        help="Path to the YAML config file",
+    )
+    args = parser.parse_args()
+
+    base_path = os.getcwd()
+
+    config_path = os.path.join(base_path, "configs", args.config_file)
+
+    config = load_config(config_path)
+
+    # Allocate config variables
+    program = config["program"]
+    data = config["data"]
+    model = config["model"]
+    training = config["training"]
+
+    seed = program["seed"]
+    gen_rand_seed = program["gen_rand_seed"]
+    gpu = program["gpu"]
+
+    shape = data["shape"]
+    test_ratio = data["test_ratio"]
+    val_ratio = data["val_ratio"]
+
+    model_id = model["model_id"]
+    number_channels = model["number_channels"]
+    number_classes = model["number_classes"]
+
+    batch_size = training["batch_size"]
+    epochs = training["epochs"]
+    learning_rate = training["learning_rate"]
+    epsilon = training["epsilon"]
+    grad_epsilon = training["grad_epsilon"]
+    clip_factor = training["clip_factor"]
+
+    print(f"Seed: {seed}")
+    print(f"Gen Rand Seed: {gen_rand_seed}")
+    print(f"GPU: {gpu}")
+    print(f"Shape: {shape}")
+    print(f"Test Ratio: {test_ratio}")
+    print(f"Validation Ratio: {val_ratio}")
+    print(f"Model ID: {model_id}")
+    print(f"Number of Channels: {number_channels}")
+    print(f"Number of Classes: {number_classes}")
+    print(f"Batch Size: {batch_size}")
+    print(f"Epochs: {epochs}")
+    print(f"Learning Rate: {learning_rate}")
+    print(f"Epsilon: {epsilon}")
+    print(f"Gradient Epsilon: {grad_epsilon}")
+    print(f"Clip Factor: {clip_factor}")
+
+    # Set device
+    if torch.cuda.is_available():
+        print(torch.cuda.get_device_name(0))
+    else:
+        print("CUDA is not available. No GPU device found.")
+
+    device = "cpu"
+
+    if gpu:
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    # Get number of workers for CPU
+    num_workers = os.cpu_count()
+
+    # Calculate dataset mean and std
+    trainset_mean_std = datasets.CIFAR10(
+        root="./data", train=True, download=True, transform=transforms.ToTensor()
+    )
+
+    mean, std = data_mean_std(trainset_mean_std)
+
+    print(f"Dataset Mean (r,g,b): {mean}")
+    print(f"Dataset STD (r,g,b): {std}")
+
+    # Prepare data
+    transform = transforms.Compose(
+        [transforms.ToTensor(), transforms.Normalize(mean, std)]
+    )
+
+    train_dataset = datasets.CIFAR10(root="data", train=True, transform=transform)
+
+    test_dataset = datasets.CIFAR10(root="data", train=False, transform=transform)
+    
+    dataset_summery(train_dataset, test_dataset)
+
+    train_loader, val_loader = get_train_val_loaders(
+        train_dataset, val_ratio, batch_size, num_workers, seed
+    )
+
+    test_loader = get_test_loader(test_dataset, batch_size, num_workers)
---
+++ utils.py
@@ -0,0 +1,90 @@
+import yaml
+import torch
+from torch.utils.data import DataLoader, random_split
+from collections import Counter
+
+
+def load_config(config_path):
+    with open(config_path, "r") as file:
+        return yaml.safe_load(file)
+
+
+def data_mean_std(cifar_trainset):
+
+    imgs = [item[0] for item in cifar_trainset]
+    imgs = torch.stack(imgs, dim=0).numpy()
+
+    # calculate mean over each channel (r,g,b)
+    mean_r = imgs[:, 0, :, :].mean()
+    mean_g = imgs[:, 1, :, :].mean()
+    mean_b = imgs[:, 2, :, :].mean()
+
+    # calculate std over each channel (r,g,b)
+    std_r = imgs[:, 0, :, :].std()
+    std_g = imgs[:, 1, :, :].std()
+    std_b = imgs[:, 2, :, :].std()
+
+    mean = [mean_r, mean_g, mean_b]
+    std = [std_r, std_g, std_b]
+
+    return mean, std
+
+
+def dataset_summery(train_dataset, test_dataset):
+    num_samples = len(train_dataset)
+    image_size = train_dataset[0][0].size()
+    class_distribution = Counter(label for _, label in train_dataset)
+    sorted_class_distribution = dict(sorted(class_distribution.items()))
+
+    print("Train Dataset -----")
+    print(f"Number of samples: {num_samples}")
+    print(f"Image size: {image_size}")
+    print(f"Class distribution: {sorted_class_distribution}")
+
+    num_samples = len(test_dataset)
+    image_size = test_dataset[0][0].size()
+    class_distribution = Counter(label for _, label in test_dataset)
+    sorted_class_distribution = dict(sorted(class_distribution.items()))
+
+    print("Test Dataset -----")
+    print(f"Number of samples: {num_samples}")
+    print(f"Image size: {image_size}")
+    print(f"Class distribution: {sorted_class_distribution}")
+
+
+def get_train_val_loaders(train_dataset, val_ratio, batch_size, num_workers, seed):
+    total_train_size = len(train_dataset)
+    val_size = int(val_ratio * total_train_size)
+    train_size = total_train_size - val_size
+
+    generator = torch.Generator().manual_seed(seed)
+    train_subset, val_subset = random_split(
+        train_dataset, [train_size, val_size], generator=generator
+    )
+
+    train_loader = DataLoader(
+        train_subset,
+        batch_size=batch_size,
+        shuffle=True,
+        num_workers=num_workers,
+    )
+
+    val_loader = DataLoader(
+        val_subset,
+        batch_size=batch_size,
+        shuffle=False,
+        num_workers=num_workers,
+    )
+
+    return train_loader, val_loader
+
+
+def get_test_loader(test_dataset, batch_size, num_workers):
+    test_loader = DataLoader(
+        test_dataset,
+        batch_size=batch_size,
+        shuffle=True,
+        num_workers=num_workers,
+        pin_memory=True,
+    )
+    return test_loader
